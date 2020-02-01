f0ceb9f0fd88de81c52d88b5f5e4d74e86dcc785
tihangrai <tihangrai@gmail.com>
2020-02-01 00:52:29 -0500

    route

--- src/server/index.js
+++ src/server/index.js
@@ -42,7 +42,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}../../../dist/index.html`));
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}../../../dist/index.html`));
 });
 
